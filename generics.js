4983fd8688c7a35d412c3ade78468c4463790d11
Stas Rivkin <rsl14591@gmail.com>
2022-09-28 18:17:17 +0300

    test

--- generics.js
+++
@@ -1,7 +0,0 @@
-const arr1 = [2, 3, 5, 7];
-const arr2 = ['one', 'two', 'three'];
-function reverseArray(arr) {
-    return arr.reverse();
-}
-const res1 = reverseArray(arr1);
-const res2 = reverseArray(arr2);
--- generics.ts
+++
@@ -1,9 +0,0 @@
-const arr1 = [2,3,5,7];
-const arr2 = ['one', 'two', 'three'];
-
-function  reverseArray<T> (arr : T[]) : T[] {
-    return arr.reverse();
-}
-
-const res1 = reverseArray(arr1);
-const res2 = reverseArray(arr2);
