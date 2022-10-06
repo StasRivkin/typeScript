bd637dba35c4f320bec8717bb19736c43063860b
Stas Rivkin <rsl14591@gmail.com>
2022-10-06 10:46:59 +0300

    repaired with two methods, homework

--- src/reducers/weatherReducer.tsx
+++ src/reducers/weatherReducer.tsx
@@ -10,32 +10,32 @@ interface Weather{
 
 const defaultWeather = {} as Weather;
 
-const weatherSlice = createSlice({
-    name: 'weather',
-    initialState: defaultWeather,
-    reducers: {
-        putWeatherInfo(weatherInfo, action){
-           weatherInfo.temp = action.payload.main.temp;
-           weatherInfo.city = action.payload.name;
-           weatherInfo.country = action.payload.sys.country;
-           weatherInfo.pressure = action.payload.main.pressure;
-           weatherInfo.sunset = action.payload.sys.sunset;
-        }
-    }
-})
-
-export const {putWeatherInfo} = weatherSlice.actions;
-export default weatherSlice.reducer;
-
 // const weatherSlice = createSlice({
 //     name: 'weather',
 //     initialState: defaultWeather,
 //     reducers: {
 //         putWeatherInfo(weatherInfo, action){
-//            return action.payload;
+//            weatherInfo.temp = action.payload.main.temp;
+//            weatherInfo.city = action.payload.name;
+//            weatherInfo.country = action.payload.sys.country;
+//            weatherInfo.pressure = action.payload.main.pressure;
+//            weatherInfo.sunset = action.payload.sys.sunset;
 //         }
 //     }
 // })
 
 // export const {putWeatherInfo} = weatherSlice.actions;
 // export default weatherSlice.reducer;
+
+const weatherSlice = createSlice({
+    name: 'weather',
+    initialState: defaultWeather,
+    reducers: {
+        putWeatherInfo(weatherInfo, action){
+           return action.payload;
+        }
+    }
+})
+
+export const {putWeatherInfo} = weatherSlice.actions;
+export default weatherSlice.reducer;
